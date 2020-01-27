68fe671b2cf91f2b8bb66fbdd706c2d13c8e025d
Amit Kachroo <amit.kachroo1987@gmail.com>
2020-01-26 19:52:51 -0600

    csv reading file

---
+++ dataPreprocessing/csvDataProcessing.py
@@ -0,0 +1,25 @@
+"""
+    Author: Amit Kachroo
+    --Class for csv reading
+    email : amitkac@gmail.com
+"""
+import pandas as pd
+import numpy as np
+
+
+class fileRead():
+
+    def __init__(self, fin, colNum, skRow):
+        self.fin = fin
+        self.colNum = colNum
+        self.skRow = skRow
+        # self.csvRead()
+
+    @classmethod
+    def csvRead(cls, self):
+        print('1. reading "{}"------------------------'.format(self.fin))
+        df = pd.read_csv(self.fin, skiprows=self.skRow, usecols=[self.colNum],
+                         dtype=np.float32)
+        df = df.dropna()
+        # print(df.head())
+        self.df = df
